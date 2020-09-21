ad67fc5b522d68dfb9adb068a354fcd4fc795dac
Alex Dong <support@alothemes.com>
2020-09-22 06:47:56 +0700

    Create emulateAreaCode.php

---
+++ emulateAreaCode.php
@@ -0,0 +1,36 @@
+<?php
+ini_set('display_startup_errors', 1);ini_set('display_errors', 1); error_reporting(-1);
+use Magento\Theme\Model\Theme\Collection;
+use Magento\Framework\App\Area;
+
+require dirname(__FILE__) . '/app/bootstrap.php';
+$bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
+
+class changeConfig extends \Magento\Framework\App\Http
+    implements \Magento\Framework\AppInterface {
+    public function launch()
+    {
+
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $appState = $objectManager->get('Magento\Framework\App\State');
+        $field = $appState->emulateAreaCode('adminhtml', function($path){
+            $bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $appState = $objectManager->get('Magento\Framework\App\State');
+            $appState->setAreaCode('adminhtml');
+            $field = $objectManager->create('Magento\Config\Model\Config\Structure')
+                ->getElementByConfigPath($path);
+            return $field;
+        }, ['customer/create_account/auto_group_assign']);
+
+        echo $field->getData()['source_model'];
+
+        return $this->_response;
+    }
+
+
+}
+
+/** @var \Magento\Framework\App\Http $app */
+$app = $bootstrap->createApplication('changeConfig');
+$bootstrap->run($app);
