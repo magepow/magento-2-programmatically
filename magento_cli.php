aec086fef3a0d64dccec27723f7dce5fef528666
Alex Dong <team.magiccart@gmail.com>
2021-04-10 14:21:34 +0700

    Update magento_cli.php

--- magento_cli.php
+++ magento_cli.php
@@ -1,28 +1,40 @@
 <?php
 
-use Magento\Framework\App\Bootstrap;
+ini_set('display_startup_errors', 1);ini_set('display_errors', 1); error_reporting(-1);
 
-require __DIR__ . '/app/bootstrap.php';
+$file = dirname(__FILE__) . '/hoi/app/bootstrap.php';
+$file = str_replace('/public_html/', '/public_htmls/', $file);
+require $file;
+$bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
 
-$bootstrap = Bootstrap::create(BP, $_SERVER);
-
-$obj = $bootstrap->getObjectManager();
-
-$state = $obj->get('Magento\Framework\App\State');
-$state->setAreaCode('frontend');
-$k[0]='bin/magento';
-$k[1]='cache:flush'; // write your proper command like setup:upgrade,cache:enable etc...
-$_SERVER['argv']=$k;
-try {
-    $handler = new \Magento\Framework\App\ErrorHandler();
-    set_error_handler([$handler, 'handler']);
-    $application = new Magento\Framework\Console\Cli('Magento CLI');
-    $application->run();
-} catch (\Exception $e) {
-    while ($e) {
-        echo $e->getMessage();
-        echo $e->getTraceAsString();
-        echo "\n\n";
-        $e = $e->getPrevious();
+class MagentoCLI extends \Magento\Framework\App\Http
+    implements \Magento\Framework\AppInterface {
+    public function launch()
+    {
+        $this->_state->setAreaCode('frontend'); 
+        $k[0]='bin/magento';
+        $k[1]='cache:flush'; // You can change command as you want like setup:static-content:deploy, cache:status etc.
+        $_SERVER['argv']=$k;
+        try {
+            $handler = new \Magento\Framework\App\ErrorHandler();
+            set_error_handler([$handler, 'handler']);
+            $application = new Magento\Framework\Console\Cli('Magento CLI');
+            $application->run();
+            echo 'done Command';
+        } catch (\Exception $e) {
+            while ($e) {
+                echo $e->getMessage();
+                echo $e->getTraceAsString();
+                echo "\n\n";
+                $e = $e->getPrevious();
+            }
+        }
+        //the method must end with this line
+        return $this->_response;
     }
+
 }
+
+/** @var \Magento\Framework\App\Http $app */
+$app = $bootstrap->createApplication('MagentoCLI');
+$bootstrap->run($app);
