ce1f927662816c6370d614319ec9e55aa3a9ad07
Alex Dong <team.magiccart@gmail.com>
2024-03-30 09:36:57 +0700

    Update ProductSalableQuantity.php

--- ProductSalableQuantity.php
+++ ProductSalableQuantity.php
@@ -19,14 +19,18 @@ setProductQty($objectManager);
 
 function setProductQty($objectManager)
 {
-    $productId = ["193206"];
-    foreach ($productId as $itemId) {
+    $productCollection = $objectManager->get('\Magento\Catalog\Model\ResourceModel\Product\CollectionFactory')->create();
+    $productCollection->addAttributeToSelect(['name, sku']);
+    $qty = 100;
+    foreach ($productCollection as $product) {
         try {
+            $productId  = $product->getId();
             $stockModel = $objectManager->get('Magento\CatalogInventory\Model\Stock\ItemFactory')->create();
             $stockResource = $objectManager->get('Magento\CatalogInventory\Model\ResourceModel\Stock\Item');
-            $stockResource->load($stockModel, $itemId,"product_id");
-            $stockModel->setQty("90");
+            $stockResource->load($stockModel, $productId,"product_id");
+            $stockModel->setQty($qty);
             $stockResource->save($stockModel);
+            echo __("Successfully updated Qty for product SKU %1", $product->getSku()) . PHP_EOL;
         } catch (Exception $e) {
             echo $e->getMessage();
         }
